259b036527e7815f7a14a98950c63617267d723f
John Heimkes IV <admin@johnheimkes.com>
2011-05-11 22:20:08 -0500

    initial commit after creating new repo

---
+++ footer.php
@@ -0,0 +1,29 @@
+     </div>
+      <div id="mast-footer">
+        <div id="footer">
+          <div id="foot-contact">
+            <div class="foot-block">
+              <h3>Mark Mazur</h3>
+              <img src="http://placekitten.com/165/280" alt="kitty!!!!!!!!" />
+              <a href="#">email@email.com</a>
+              <a href="http://twitter.com/markdmazur" target="_blank">@markdmazur</a>
+            </div>
+            <div class="foot-block">
+              <h3>Trent Hilborn</h3>
+              <img src="http://placekitten.com/165/280" alt="kitty!!!!!!!!" />
+              <a href="#">email@email.com</a>
+              <a href="#" target="_blank">@twitter</a>
+            </div>
+            <div class="foot-block">
+              <h3>Lorem Ipsum</h3>
+              <img src="http://placekitten.com/165/280" alt="kitty!!!!!!!!" />
+              <a href="#">email@email.com</a>
+              <a href="#" target="_blank">@twitter</a>
+            </div>
+          </div>
+          <p class="copy">&copy; 2011 The First Feature</p>
+        </div>
+      </div>  
+    </div>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>The First Feature</title>
+    <meta name="robots" content="index,all" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css" media="screen" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/images/favicon.ico" />
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+  </head>
+  <body <?php body_class(); ?>>
+    <div id="wrapper">
+      <div id="mast-head">
+        <div id="header">
+          <h1><a href="<?php bloginfo('url') ?>">The First Feature</a></h1>
+        </div>
+      </div>
+      <div id="mast-nav">
+        <div id="nav">
+          <ul>
+            <li><a href="<?php bloginfo('url') ?>">Home</a></li>
+            <li><a href="<?php bloginfo('url') ?>/the-beginning">The Beginning</a></li>
+            <li><a href="<?php bloginfo('url') ?>/life-doesnt-wait">Life Doesn't Wait</a></li>
+            <li><a href="<?php bloginfo('url') ?>/untitled">Untitled</a></li>
+            <li><a href="<?php bloginfo('url') ?>/episodes">Episodes</a></li>
+            <li><a href="<?php bloginfo('url') ?>/about">About</a></li>
+            <li><a href="<?php bloginfo('url') ?>/donate">Donate</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="mast-content">
