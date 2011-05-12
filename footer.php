2bf94f96b4678c4ef35e8f6ee692f98c0c21306c
John Heimkes IV <admin@johnheimkes.com>
2011-05-12 18:43:50 -0500

    pushing to papabear

--- footer.php
+++ footer.php
@@ -20,6 +20,26 @@
               <a href="#">email@email.com</a>
               <a href="#" target="_blank">@twitter</a>
             </div>
+            <div id="foot-contact">
+              <a href="#" target="_blank">Twitter</a>
+              <a href="#" target="_blank">Facebook</a>
+              <a href="#" target="_blank">Donate</a>
+              <?php
+                if($_GET['success'] == true) {
+                  echo "<p class='return_pass'>Thanks! We'll get back to you soon.</p>";
+                } elseif($_GET['error'] == true) {
+                  echo "<p class='return_fail'>Error! Did you forget something?</p>";
+                }
+              ?>
+              <fieldset>
+                <form action="form.php" method="post">
+                  <input type="text" name="name" value="Name" id="name" class="clearit" />
+                  <input type="text" name="email" value="Email" id="email" class="clearit" />
+                  <textarea name="message" id="message" value="Message" class="clearit"></textarea>
+                  <input type="submit" name="submit" value="Send" id="submit" />
+                </form>
+              </fieldset>
+            </div>
           </div>
           <p class="copy">&copy; 2011 The First Feature</p>
         </div>
